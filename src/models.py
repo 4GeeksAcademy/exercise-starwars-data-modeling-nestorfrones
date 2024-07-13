d96665463047f217fbe9a2937e67e8b03e6d8185
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-07-13 03:35:00 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -9,24 +9,49 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    suscription_date =  Column(String(250), nullable=False)
+    favorites = relationship('favorite')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    manofacturer = Column(String(50), nullable=False)
+    
 
-    def to_dict(self):
-        return {}
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    birth_year = Column(String(50), nullable=False)
+    gender = Column(String(50), nullable=False)
+    height = Column(String(50), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    climate = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+
+    
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    favorite_id = Column(String(250), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    persons = relationship('person')
+    vehicles = relationship('vehicle')
+    planets = relationship('planet')
+    persons = relationship('person')
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
